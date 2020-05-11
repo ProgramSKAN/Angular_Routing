83b90ff8f23e81f1a92880061d801d5fa8c10d39
SandeepKandagatla <kandagatlas@gmail.com>
2020-05-11 19:55:31 +0530

    lazy loading::add auth guard in the place where we load lazy module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { WelcomeComponent } from "./home/welcome.component";
 import { PageNotFoundComponent } from "./page-not-found.component";
+import { AuthGuard } from './user/auth.guard';
 
 const routes: Routes = [];
 
@@ -11,6 +12,7 @@ const routes: Routes = [];
       { path: "welcome", component: WelcomeComponent },
       {
         path: 'products',
+        canActivate: [AuthGuard],
         loadChildren: () =>
           import('./products/product.module').then(m => m.ProductModule)
       },
